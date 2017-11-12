a01d2d4ed1ed8faa69158d88d5100ee2c02c7402
uaithalkota <ullasaithalk@gmail.com>
2017-11-11 21:02:33 -0600

    Added User info

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet,View, Button } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 export default class LinksScreen extends React.Component {
@@ -10,12 +10,66 @@ export default class LinksScreen extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+        <View>
+          <Button onPress={this._getUserInformation}
+          title = "Press Me"
+          />
+          </View>
       </ScrollView>
     );
   }
+  _getUserInformation = async() => {
+
+      let uploadResponse, uploadResult;
+    try {
+     
+
+
+       uploadResponse = await getInformation();
+       uploadResult = await uploadResponse.json();
+      console.log(uploadResult);   
+
+       
+
+
+       
+
+
+   } catch (e) {
+     console.log({ uploadResponse });
+     console.log({ uploadResult });
+     console.log({ e });
+     
+   } 
+  }
+}
+async function getInformation() {
+
+  let apiUrl = 'http://104.194.98.94:8080/users';
+
+      // Note:
+      // Uncomment this if you want to experiment with local server
+      //
+      // if (Constants.isDevice) {
+      //   apiUrl = `https://your-ngrok-subdomain.ngrok.io/upload`;
+      // } else {
+      //   apiUrl = `http://localhost:3000/upload`
+      // }
+
+    //  let uriParts = uri.split('.');
+    //  let fileType = uri[uri.length - 1];
+
+
+      let options = {
+        method: 'GET',
+        body: formData,
+        headers: {
+          Accept: 'application/json',
+          
+        },
+      };
+      return fetch(apiUrl, options);
+
 }
 
 const styles = StyleSheet.create({
